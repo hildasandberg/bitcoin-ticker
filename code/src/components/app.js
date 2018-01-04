2026a7725236a66963fc80cb4eb1b096bb66e932
Hilda <hilda.sandberg@gmail.com>
2018-01-04 11:52:12 +0100

    local storage added but got a bug with id

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { LineChart, Line, Tooltip, YAxis } from "recharts"
 import openGdaxWebsocket from "../gdax-websocket"
 
 class App extends React.Component {
@@ -19,20 +20,31 @@ class App extends React.Component {
   }
 
   handleNewTickerMessage = newTickerMessage => {
+    newTickerMessage.price = parseInt(newTickerMessage.price, 10)
     this.setState(previousState => ({
       tickerMessages: previousState.tickerMessages.concat([newTickerMessage])
     }))
   }
 
+  // render() {
+  //   return (
+  //     <div>
+  //       {this.state.tickerMessages.map(msg => (
+  //         <div key={msg.sequence}>
+  //           {msg.time}: <strong>{msg.price} EUR</strong>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   )
+  // }
+
   render() {
     return (
-      <div>
-        {this.state.tickerMessages.map(msg => (
-          <div key={msg.sequence}>
-            {msg.time}: <strong>{msg.price} EUR</strong>
-          </div>
-        ))}
-      </div>
+      <LineChart width={800} height={400} data={this.state.tickerMessages}>
+        <Line type="monotone" dataKey="price" stroke='#8884d8' strokeWidth={2} />
+        <YAxis type="number" domain={['dataMin', 'dataMax']} />
+        <Tooltip />
+      </LineChart>
     )
   }
 
